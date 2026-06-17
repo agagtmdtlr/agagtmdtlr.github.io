37ec3070525042645e007812e2eafbc33b70727c
박승식 <briankunkel9@gmail.com>
2026-06-17 18:39:36 +0900

    기존 페이지 삭제
    jekyll 기반으로 작업하기위해 기존 페이지 삭제

--- docs/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-import BlogApp from './BlogApp.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    const blog = new BlogApp();
-    blog.init().catch(err => {
-        console.error("블로그 앱 초기화 실패:", err);
-    });
-});
